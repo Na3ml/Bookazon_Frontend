3c61e5b6141707d73b0dc1da910c81d1c63b4f26
Ahmed Zakaria <ahmedzakria102@gmail.com>
2023-08-11 21:17:55 +0300

    first upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PopularHotel from "./components/popularHotel";
+import Offers from "./components/offers/Offers";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
-   
       <Home />
- 
+      <Offers />
     </div>
   );
 };
