b1582fe2f127c5164b9836afd9bbc2bbf0f9d60d
Mohamed <bola13368@gmail.com>
2023-08-19 02:28:21 +0300

    hotel-slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import PopularHotel from "./components/popularHotel";
-import Offers from "./components/offers/Offers";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/login/Login.jsx";
 import SignIn from "./pages/SignIn";
@@ -15,7 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
