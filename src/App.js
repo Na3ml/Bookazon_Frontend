bb4f33210f3453293e3565f960e1fb2899750c2a
Amgad Mohamed <amgagmohamed38@gmail.com>
2023-08-19 04:52:47 +0300

    Merge branch 'main' into nav_deals-reviews

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PopularHotel from "./components/popularHotel";
-import Offers from "./components/offers/Offers";
+// import PopularHotel from "./components/popularHotel";
+// import Offers from "./components/offers/Offers";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
