92ff6fef18304ba6a7fddd336f7b6a90deb58300
mac <abdallahmohamedien@gmail.com>
2023-08-31 04:39:07 +0300

    search-edit

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -11,21 +11,21 @@ import './Search.css';
 const index = () => {
 
 
-  const url =""
+  const url ="https://bookazon.tadafoq.com/api/home/search"
   const [data,setData] = useState({
-    location: "",
-    datein: "",
-    dateout: "",
-    geusts: "",
+    city: "",
+    check_in: "",
+    check_out: "",
+    total_guests: "",
   })
 
   function submit(e){
     e.preventDefault();
     Axios.post(url,{
-      location: data.location,
-      datein: data.datein,
-      dateout: data.dateout,
-      geusts: data.geusts,
+      city: data.city,
+      check_in: data.check_in,
+      check_out: data.check_out,
+      total_guests: data.total_guests,
     })
     .then(res=>{
       console.log(res.data)
@@ -42,39 +42,41 @@ const index = () => {
   return (
     <div>
       <div className='home'>
-        <div className='container' >
+        <div className='container'>
           <span className='d-flex'>
-            <h1 className='text-white  '>Enjoy Your Dream <br /> Vacation</h1>
+            <h1 className='text-white'>Enjoy Your Dream <br className="br-s"/> Vacation</h1>
           </span>
             <div className="headerSearch ">
-              <form onSubmit={(e) => submit(e)} className='form-search' >
+              <form method='post' onSubmit={(e) => submit(e)} className='form-search' >
                 <div className="d-flex location">
                   <HiOutlineLocationMarker className='icon text-white'/>
-                  <input type="search" onChange={(e) => handel(e)} placeholder='Where To?' id="location" className='input1 text-white' />
+                  <input type="search" onChange={(e) => handel(e)} placeholder='Where To?' id="city" className='input1 text-white' />
                 </div>
-                <div className="check d-flex">
+                <div className="check d-flex align-items-center p-2">
                   <BsCalendarWeek className='icon1 text-white' />
-                  <span className='d-grid '>
-                    <span className='text-white'>
+                  <div>
+                  <span className='d-grid'>
+                    <span className='text-white h-5 mt-1'>
                       Check in 
                     </span>
-                    <input type="date" onChange={(e) => handel(e)} id="datein" className="input2 text-white" size={16}/>
+                    <input type="date" onChange={(e) => handel(e)} id="check_in" className="input2 text-white" size={16}/>
                   </span>
+                  </div>
                 </div>
                 <div className="check d-flex p-2">
-                <BsCalendarWeek className='icon1 text-white'/>
+                <BsCalendarWeek className='icon2 text-white'/>
                   <span className='d-grid'>
                     <span className='text-white h-5 mt-1'>
                       Check out
                     </span>
-                    <input type="date" onChange={(e) => handel(e)} id="dateout" className="input2 text-white"  size={16}/>
+                    <input type="date" onChange={(e) => handel(e)} id="check_out" className="input2 text-white"  size={16}/>
                   </span>
                 </div>
                 <div className="check d-flex p-2">
-                  <BsPerson className='icon1 text-white' />
+                  <BsPerson className='icon3 text-white' size={22}/>
                   <div className='d-grid'>
                     <label htmlFor="gaustes" className='text-white mt-1'>Geusts</label>
-                      <select name="" onChange={(e) => handel(e)} id="geusts" className='select text-white'>
+                      <select name="" onChange={(e) => handel(e)} id="total_guests" className='select text-white'>
                         <option className='' value="1">1 Geusts</option>
                         <option value="2">2 Geusts</option>
                         <option value="3">3 Geusts</option>
@@ -89,7 +91,7 @@ const index = () => {
               </form>
               
             </div>
-            <div className="text-white mt-3">
+            <div className="user-avatar text-white mt-3">
               <Stack spacing={1}>
                 <Stack direction='row' spacing={-1}>
                   <Avatar sx={{ bgcolor: 'primary.light' }}>FE</Avatar>
